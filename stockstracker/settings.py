05365e4b829e49e5451303ca8b286a315b619b29
Nicholas <nicholas5538@gmail.com>
2022-11-27 21:19:46 +0800

    Update Hosting URLs under ALLOWED_HOSTS

--- stockstracker/settings.py
+++ stockstracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['stockstracker-app.herokuapp.com']
+ALLOWED_HOSTS = ['stockstrackerapp.com', 'www.stockstrackerapp.com', 'stockstracker.up.railway.app']
 
 
 # Application definition
