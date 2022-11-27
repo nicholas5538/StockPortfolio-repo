ac0d2a2f96cbbba5b311d7587b5cd7eb587181ac
Nicholas <nicholas5538@gmail.com>
2022-11-27 20:54:07 +0800

    Changing DB credentials from Heroku to railway

--- stockstracker/settings.py
+++ stockstracker/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'USER': os.environ.get('DATABASE_USER'),
         'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
         'HOST': os.environ.get('DATABASE_HOST'),
-        'PORT': '5432'
+        'PORT': '6017'
     }
 }
 
