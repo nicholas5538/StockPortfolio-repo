91cdb7934ef6f1d86e7178618b3ba478691a3cc3
Nicholas <nicholas5538@gmail.com>
2022-11-27 21:51:03 +0800

    Troubleshoot staticfiles issues (Error 500)

--- stockstracker/settings.py
+++ stockstracker/settings.py
@@ -24,8 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-ALLOWED_HOSTS = ['stockstrackerapp.com', 'www.stockstrackerapp.com', 'stockstracker.up.railway.app']
+DEBUG = False
+ALLOWED_HOSTS = ['stockstracker.up.railway.app', 'stockstrackerapp.com', 'www.stockstrackerapp.com']
 
 
 # Application definition
@@ -126,7 +126,7 @@ USE_L10N = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
