6756fe825d9455938c707733e10263124b9f5af5
Nicholas <nicholas5538@gmail.com>
2022-11-27 21:59:34 +0800

    Remove whitenoise compression

--- stockstracker/settings.py
+++ stockstracker/settings.py
@@ -126,7 +126,7 @@ USE_L10N = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
