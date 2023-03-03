1cbe7887c153418a12380e6bd63e2e3b46cc2d62
nicholas5538 <nicholas5538@gmail.com>
2023-03-03 09:58:45 +0800

    Change to vercel deployment

--- stockstracker/settings.py
+++ stockstracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['stockstracker.up.railway.app', 'stockstrackerapp.com', 'www.stockstrackerapp.com']
+ALLOWED_HOSTS = ['.vercel.app', 'stockstrackerapp.com', 'www.stockstrackerapp.com']
 
 
 # Application definition
